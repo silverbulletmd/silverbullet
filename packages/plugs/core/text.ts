86823918cceaf7b7d6c44ab3577c31efcc8cbab2
Guillermo Vaya <guivaya@gmail.com>
2022-07-10 17:51:34 +0200

    Add link command

--- packages/plugs/core/text.ts
+++ packages/plugs/core/text.ts
@@ -56,6 +56,23 @@ export async function numberListifySelection() {
   await replaceRange(from, selection.to, text);
 }
 
+export async function linkSelection() {
+  const text = await getText();
+  const selection = await getSelection();
+  const textSelection = text.slice(selection.from, selection.to);
+  let linkedText = `[]()`;
+  if (textSelection.length > 0) {
+    try {
+      new URL(textSelection);
+      linkedText = `[](${textSelection})`;
+    } catch {
+      linkedText = `[${textSelection}]()`;
+
+    }
+  }
+  await replaceRange(selection.from, selection.to, linkedText)
+}
+
 export function wrapSelection(cmdDef: any) {
   return insertMarker(cmdDef.wrapper);
 }
