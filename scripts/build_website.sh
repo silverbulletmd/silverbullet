c096067687c4067e9f2f599da94510a4fca09e6a
Zef Hemel <zef@zef.me>
2022-07-15 11:51:54 +0200

    Another iterations

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -4,4 +4,6 @@ npm run clean-build
 rm -rf website
 mkdir -p website/fs
 cp -r packages/web/dist/* website/
+cp _redirects website/
 cp -r docs/* website/fs/
+node scripts/generate_fs_list.js > website/fs/index.json
---
+++ scripts/generate_fs_list.js
@@ -0,0 +1,29 @@
+const { resolve } = require("path");
+const { readdir } = require("fs").promises;
+
+async function getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    })
+  );
+  return Array.prototype.concat(...files);
+}
+
+const rootDir = resolve("docs");
+
+getFiles(rootDir).then((files) => {
+  files = files
+    .map((file) => ({
+      name: file.substring(rootDir.length + 1).replace(/\.md$/, ""),
+      lastModified: 0,
+      perm: "ro",
+    }))
+    .filter(
+      (pageMeta) =>
+        !pageMeta.name.startsWith("_") && !pageMeta.name.startsWith(".")
+    );
+  console.log(JSON.stringify(files, null, 2));
+});
