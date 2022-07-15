4ce22ff453f5264737576bf8e682e354030587c0
Zef Hemel <zef@zef.me>
2022-07-15 14:14:26 +0200

    Build

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -2,10 +2,11 @@
 
 npm run clean-build
 rm -rf website
-mkdir -p website/fs
+mkdir -p website/fs/_plug
 cp -r packages/web/dist/* website/
 cp _redirects website/
 cp -r docs/* website/fs/
+cp packages/plugs/dist/* website/fs/_plug/
 find website/fs/ -depth -name "*.md" -exec sh -c 'mv "$1" "${1%.md}"' _ {} \;
 find website/fs/ -depth -name "*.plug.json" -exec sh -c 'mv "$1" "${1%.plug.json}"' _ {} \;
 node scripts/generate_fs_list.js > website/index.json
