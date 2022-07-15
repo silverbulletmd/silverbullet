026aa3fa8bfc19c9edad61ae280400ca0a9926b9
Zef Hemel <zef@zef.me>
2022-07-15 12:00:30 +0200

    Build

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -6,4 +6,6 @@ mkdir -p website/fs
 cp -r packages/web/dist/* website/
 cp _redirects website/
 cp -r docs/* website/fs/
-node scripts/generate_fs_list.js > website/fs/index.json
+find website/fs/ -name "*.md" -exec bash -c 'mv "$1" "${1%.md}"' - '{}' +
+find website/fs/ -name "*.json" -exec bash -c 'mv "$1" "${1%.json}"' - '{}' +
+node scripts/generate_fs_list.js > website/index.json
