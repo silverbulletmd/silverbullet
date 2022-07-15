89c61261c2dd8fb030495028a048d18c06a14cda
Zef Hemel <zef@zef.me>
2022-07-15 12:04:44 +0200

    Build

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-npm run clean-build
+#npm run clean-build
 rm -rf website
 mkdir -p website/fs
 cp -r packages/web/dist/* website/
 cp _redirects website/
 cp -r docs/* website/fs/
-find website/fs/ -name "*.md" -exec bash -c 'mv "$1" "${1%.md}"' - '{}' +
-find website/fs/ -name "*.json" -exec bash -c 'mv "$1" "${1%.json}"' - '{}' +
+find website/fs/ -depth -name "*.md" -exec sh -c 'mv "$1" "${1%.md}"' _ {} \;
+find website/fs/ -depth -name "*.plug.json" -exec sh -c 'mv "$1" "${1%.plug.json}"' _ {} \;
 node scripts/generate_fs_list.js > website/index.json
