9f7b59905ac386e806eccd3e12103c5f7226e798
Zef Hemel <zef@zef.me>
2022-08-02 12:44:41 +0200

    Attempt to fix line wrapper bugs

--- packages/web/line_wrapper.ts
+++ packages/web/line_wrapper.ts
@@ -18,43 +18,45 @@ interface WrapElement {
 function wrapLines(view: EditorView, wrapElements: WrapElement[]) {
   let widgets: Range<Decoration>[] = [];
   let elementStack: string[] = [];
-  for (let { from, to } of view.visibleRanges) {
-    const doc = view.state.doc;
-    syntaxTree(view.state).iterate({
-      from,
-      to,
-      enter: ({ type, from, to }) => {
-        for (let wrapElement of wrapElements) {
-          if (type.name == wrapElement.selector) {
+  const doc = view.state.doc;
+  // Disabling the visible ranges for now, because it may be a bit buggy.
+  // RISK: this may actually become slow for large documents.
+  // for (let { from, to } of view.visibleRanges) {
+  syntaxTree(view.state).iterate({
+    // from,
+    // to,
+    enter: ({ type, from, to }) => {
+      for (let wrapElement of wrapElements) {
+        if (type.name == wrapElement.selector) {
+          if (wrapElement.nesting) {
+            elementStack.push(type.name);
+          }
+          const bodyText = doc.sliceString(from, to);
+          let idx = from;
+          for (let line of bodyText.split("\n")) {
+            let cls = wrapElement.class;
             if (wrapElement.nesting) {
-              elementStack.push(type.name);
+              cls = `${cls} ${cls}-${elementStack.length}`;
             }
-            const bodyText = doc.sliceString(from, to);
-            let idx = from;
-            for (let line of bodyText.split("\n")) {
-              let cls = wrapElement.class;
-              if (wrapElement.nesting) {
-                cls = `${cls} ${cls}-${elementStack.length}`;
-              }
-              widgets.push(
-                Decoration.line({
-                  class: cls,
-                }).range(doc.lineAt(idx).from)
-              );
-              idx += line.length + 1;
-            }
+            widgets.push(
+              Decoration.line({
+                class: cls,
+              }).range(doc.lineAt(idx).from)
+            );
+            idx += line.length + 1;
           }
         }
-      },
-      leave({ type }) {
-        for (let wrapElement of wrapElements) {
-          if (type.name == wrapElement.selector && wrapElement.nesting) {
-            elementStack.pop();
-          }
+      }
+    },
+    leave({ type }) {
+      for (let wrapElement of wrapElements) {
+        if (type.name == wrapElement.selector && wrapElement.nesting) {
+          elementStack.pop();
         }
-      },
-    });
-  }
+      }
+    },
+  });
+  // }
   // Widgets have to be sorted by `from` in ascending order
   widgets = widgets.sort((a, b) => {
     return a.from < b.from ? -1 : 1;
