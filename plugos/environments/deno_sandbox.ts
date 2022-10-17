c2ccd2a6a171118fdf5345ef7108eac18718f55b
Zef Hemel <zef@zef.me>
2022-10-17 15:56:47 +0200

    Fixes #94

--- plugos/environments/deno_sandbox.ts
+++ plugos/environments/deno_sandbox.ts
@@ -46,7 +46,24 @@ export function createSandbox(plug: Plug<any>) {
     workerHref,
     {
       type: "module",
-    },
+      deno: {
+        permissions: {
+          // Allow network access and servers (main use case: fetch)
+          net: true,
+          // This is required for console loggin to work, apparently?
+          env: true,
+          // No talking to native code
+          ffi: false,
+          // No invocation of shell commands
+          run: false,
+          // No read access to the file system
+          read: false,
+          // No write access to the file system
+          write: false,
+        },
+      },
+      // Have to do this because the "deno" option is not standard and doesn't typecheck yet
+    } as any,
   );
   return new Sandbox(plug, new DenoWorkerWrapper(worker));
 }
