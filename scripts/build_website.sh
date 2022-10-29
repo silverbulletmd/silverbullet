c159badf47bcc1f63f16d1f0afdea95fbdc8681c
Zef Hemel <zef@zef.me>
2022-10-29 09:54:18 +0200

    Building SB on netlify

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -2,3 +2,10 @@
 
 rm -rf website_build
 npx --yes @silverbulletmd/publish -o website_build --index website
+
+echo "Now building Silver Bullet bundle"
+curl -fsSL https://deno.land/install.sh | sh
+export PATH=~/.deno/bin:$PATH
+deno task build
+deno task bundle
+cp dist/silverbullet.js website_build/silverbullet.js
