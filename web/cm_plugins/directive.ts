850077bad0619d5d321bbcd4ca7589e1d51c5a22
Zef Hemel <zef@zef.me>
2022-12-09 17:13:26 +0100

    Fixes #144

--- web/cm_plugins/directive.ts
+++ web/cm_plugins/directive.ts
@@ -1,44 +1,62 @@
 import { Decoration, EditorState, syntaxTree } from "../deps.ts";
-import { decoratorStateField, isCursorInRange } from "./util.ts";
+import {
+  decoratorStateField,
+  invisibleDecoration,
+  isCursorInRange,
+} from "./util.ts";
 
-function getDirectives(state: EditorState) {
-  const widgets: any[] = [];
-
-  syntaxTree(state).iterate({
-    enter: ({ type, from, to }) => {
-      if (type.name !== "CommentBlock") {
-        return;
-      }
-      const text = state.sliceDoc(from, to);
-      if (/<!--\s*#/.exec(text)) {
-        // Open directive
-        widgets.push(
-          Decoration.line({
-            class: "sb-directive-start",
-          }).range(from),
-        );
-      } else if (/<!--\s*\//.exec(text)) {
-        widgets.push(
-          Decoration.line({
-            class: "sb-directive-end",
-          }).range(from),
-        );
-      } else {
-        return;
-      }
-      if (!isCursorInRange(state, [from, to])) {
-        widgets.push(
-          Decoration.line({
-            class: "sb-directive-outside",
-          }).range(from),
-        );
-      }
-    },
-  });
-
-  return Decoration.set(widgets, true);
-}
-
+// Does a few things: hides the directives when the cursor is not placed inside
+// Adds a class to the start and end of the directive when the cursor is placed inside
 export function directivePlugin() {
-  return decoratorStateField(getDirectives);
+  return decoratorStateField((state: EditorState) => {
+    const widgets: any[] = [];
+    const cursorPos = state.selection.main.head;
+
+    // TODO: This doesn't handle nested directives properly
+    let posOfLastOpen = { from: 0, to: 0 };
+
+    syntaxTree(state).iterate({
+      enter: ({ type, from, to }) => {
+        if (type.name !== "CommentBlock") {
+          return;
+        }
+        const text = state.sliceDoc(from, to);
+        if (/<!--\s*#/.exec(text)) {
+          // Open directive
+          posOfLastOpen = { from, to };
+        } else if (/<!--\s*\//.exec(text)) {
+          // Close directive
+          if (
+            (cursorPos > to || cursorPos < posOfLastOpen.from) &&
+            !isCursorInRange(state, [posOfLastOpen.from, to])
+          ) {
+            widgets.push(
+              invisibleDecoration.range(
+                posOfLastOpen.from,
+                posOfLastOpen.to + 1,
+              ),
+            );
+            widgets.push(
+              invisibleDecoration.range(from - 1, to),
+            );
+          } else {
+            widgets.push(
+              Decoration.line({
+                class: "sb-directive-start",
+              }).range(posOfLastOpen.from),
+            );
+            widgets.push(
+              Decoration.line({
+                class: "sb-directive-end",
+              }).range(from),
+            );
+          }
+        } else {
+          return;
+        }
+      },
+    });
+
+    return Decoration.set(widgets, true);
+  });
 }
