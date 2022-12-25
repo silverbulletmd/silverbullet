d82dcff4216e11a732794b9773918260581613e7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-12-25 17:31:01 -0500

    add build_plugs.ts

--- plugos/deps.ts
+++ plugos/deps.ts
@@ -1,6 +1,7 @@
 export { globToRegExp } from "https://deno.land/std@0.165.0/path/glob.ts";
 export { walk } from "https://deno.land/std@0.165.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.165.0/path/mod.ts";
+export { expandGlobSync } from "https://deno.land/std@0.165.0/fs/mod.ts";
 export { mime } from "https://deno.land/x/mimetypes@v1.0.0/mod.ts";
 export { default as cacheDir } from "https://deno.land/x/cache_dir@0.2.0/mod.ts";
 export * as flags from "https://deno.land/std@0.165.0/flags/mod.ts";
