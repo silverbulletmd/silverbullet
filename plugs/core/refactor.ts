1d1e32321ad25cc4058274224cb36bcdd111d8ec
Tristan Sokol <tristan@rightfoot.com>
2023-01-04 16:29:15 -0500

    add in new refactor file and extract to page command;

---
+++ plugs/core/refactor.ts
@@ -0,0 +1,41 @@
+import {
+  editor,
+  space,
+} from "$sb/silverbullet-syscall/mod.ts";
+
+export async function extractToPage(cmdDef: any) {
+  console.log("Got a target name", cmdDef.page);
+  const cursor = await editor.getCursor();
+
+  const newName = cmdDef.page ||
+    await editor.prompt(`New page title:`, 'new page');
+  if (!newName) {
+    return;
+  }
+
+  console.log("New name", newName);
+
+  try {
+    // This throws an error if the page does not exist, which we expect to be the case
+    await space.getPageMeta(newName);
+    // So when we get to this point, we error out
+    throw new Error(
+      `Page ${newName} already exists, cannot rename to existing page.`,
+    );
+  } catch (e: any) {
+    if (e.message.includes("not found")) {
+      // Expected not found error, so we can continue
+    } else {
+      await editor.flashNotification(e.message, "error");
+      throw e;
+    }
+  }
+  let text = await editor.getText();
+  const selection = await editor.getSelection();
+  text = text.slice(selection.from, selection.to);
+  await editor.replaceRange(selection.from, selection.to, "");
+  console.log("Writing new page to space");
+  const newPageMeta = await space.writePage(newName, text);
+  console.log("Navigating to new page");
+  await editor.navigate(newName, cursor, true);
+}
