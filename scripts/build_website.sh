94dbb0fc791881d1bf91342c7820ab7ca9fb4795
Zef Hemel <zef@zef.me>
2023-05-09 17:39:08 +0200

    Website build

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -16,30 +16,28 @@ echo "Cleaning website build dir"
 rm -rf website_build
 mkdir -p website_build/fs/_plug
 echo "Copying silverbullet runtime files"
-cp -r dist_bundle/web/* website_build/
+cp -r dist_client_bundle/* website_build/
 
-echo "And all plugs"
-cp -r dist_bundle/_plug/* website_build/fs/_plug/
-echo "And additional ones"
-curl https://raw.githubusercontent.com/silverbulletmd/silverbullet-mermaid/main/mermaid.plug.json > website_build/fs/_plug/mermaid.plug.json
-echo "But remove some plugs"
-rm -rf website_build/fs/_plug/{directive,plugmd,publish,share}.plug.json
+#echo "And additional ones"
+#curl https://raw.githubusercontent.com/silverbulletmd/silverbullet-mermaid/main/mermaid.plug.json > website_build/fs/_plug/mermaid.plug.json
+#echo "But remove some plugs"
+#rm -rf website_build/fs/_plug/{directive,plugmd,publish,share}.plug.json
 #echo "Copying netlify config files"
 #cp website/{_redirects,_headers} website_build/
 
-echo "Copying website content into fs/"
-cp -r website/* website_build/fs/
-rm website_build/fs/{_redirects,_headers}
+#echo "Copying website content into fs/"
+#cp -r website/* website_build/fs/
+#rm website_build/fs/{_redirects,_headers}
 
 echo "Copy website files another time into the root"
 cp -r website/* website_build/
 
-echo "Generating file listing"
-deno run -A scripts/generate_fs_list.ts > website_build/index.json
+#echo "Generating file listing"
+#deno run -A scripts/generate_fs_list.ts > website_build/index.json
 
 echo > website_build/empty.md
 
-echo "Bundling..."
+#echo "Bundling..."
 deno task bundle
 cp dist/silverbullet.js website_build/
 cp dist_bundle/web/global.plug.json website_build/
