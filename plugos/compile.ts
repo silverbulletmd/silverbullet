2024a3e6d8f404f5d26ce666322a787037027b8e
Zef Hemel <zef@zef.me>
2023-05-15 20:34:43 +0200

    Changed back to linked sourcemaps

--- plugos/compile.ts
+++ plugos/compile.ts
@@ -88,7 +88,7 @@ setupMessageListener(functionMapping, manifest);
     format: "iife",
     globalName: "mod",
     platform: "browser",
-    sourcemap: options.debug ? "inline" : false,
+    sourcemap: options.debug ? "linked" : false,
     minify: !options.debug,
     outfile: outFile,
     metafile: options.info,
