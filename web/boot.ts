c746d77a6bc8f4829003cd629e88888efdd55b8b
Zef Hemel <zef@zef.me>
2023-05-24 05:42:24 +0200

    Alert when indexeddb not available

--- web/boot.ts
+++ web/boot.ts
@@ -32,3 +32,9 @@ if (navigator.serviceWorker) {
     "Not launching service worker, likely because not running from localhost or over HTTPs. This means SilverBullet will not be available offline.",
   );
 }
+
+if (!globalThis.indexedDB) {
+  alert(
+    "SilverBullet requires IndexedDB to operate and it is not available in your browser. Please use a recent version of Chrome, Firefox (not in private mode) or Safari.",
+  );
+}
