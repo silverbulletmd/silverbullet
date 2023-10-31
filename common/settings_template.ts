3d3535f56c67604360ea39a1c6aa46562a622d8f
Zef Hemel <zef@zef.me>
2023-10-31 10:48:00 +0100

    Link to settings docs

--- common/settings_template.ts
+++ common/settings_template.ts
@@ -1,5 +1,6 @@
 export const SETTINGS_TEMPLATE =
   `This page contains settings for configuring SilverBullet and its plugs. Any changes outside of the yaml block will be overwritten.
+A list of all built-in support settings [[!silverbullet.md/SETTINGS|can be found here]].
 
 \`\`\`yaml
 indexPage: index
