d0fd2a1826c77d5624f8c3564163998819e408ad
Zef Hemel <zef@zef.me>
2023-11-23 12:08:22 +0100

    root folder case

--- plugs/editor/upload.ts
+++ plugs/editor/upload.ts
@@ -4,41 +4,47 @@ import { UploadFile } from "$sb/types.ts";
 const maximumAttachmentSize = 1024 * 1024 * 10; // 10MB
 
 function folderName(path: string) {
-    return path.split("/").slice(0, -1).join("/");
+  return path.split("/").slice(0, -1).join("/");
 }
 
 async function saveFile(file: UploadFile) {
-    if (file.content.length > maximumAttachmentSize) {
-        editor.flashNotification(
-            `Attachment is too large, maximum is ${maximumAttachmentSize / 1024 / 1024
-            }MB`,
-            "error",
-        );
-        return;
-    }
-
-    const suggestedName = folderName(await editor.getCurrentPage()) + "/" + file.name;
-    console.log(suggestedName)
-
-    const finalFileName = await editor.prompt(
-        "File name for pasted attachment",
-        suggestedName,
+  if (file.content.length > maximumAttachmentSize) {
+    editor.flashNotification(
+      `Attachment is too large, maximum is ${
+        maximumAttachmentSize / 1024 / 1024
+      }MB`,
+      "error",
     );
-    if (!finalFileName) {
-        return;
-    }
-    await space.writeAttachment(
-        finalFileName,
-        file.content,
-    );
-    let attachmentMarkdown = `[${finalFileName}](${encodeURI(finalFileName)})`;
-    if (file.contentType.startsWith("image/")) {
-        attachmentMarkdown = `![](${encodeURI(finalFileName)})`;
-    }
-    editor.insertAtCursor(attachmentMarkdown);
+    return;
+  }
+
+  let prefix = folderName(await editor.getCurrentPage()) + "/";
+  if (prefix === "/") {
+    // root folder case
+    prefix = "";
+  }
+  const suggestedName = prefix + file.name;
+  console.log(suggestedName);
+
+  const finalFileName = await editor.prompt(
+    "File name for pasted attachment",
+    suggestedName,
+  );
+  if (!finalFileName) {
+    return;
+  }
+  await space.writeAttachment(
+    finalFileName,
+    file.content,
+  );
+  let attachmentMarkdown = `[${finalFileName}](${encodeURI(finalFileName)})`;
+  if (file.contentType.startsWith("image/")) {
+    attachmentMarkdown = `![](${encodeURI(finalFileName)})`;
+  }
+  editor.insertAtCursor(attachmentMarkdown);
 }
 
 export async function uploadFile() {
-    const uploadFile = await editor.uploadFile();
-    await saveFile(uploadFile);
+  const uploadFile = await editor.uploadFile();
+  await saveFile(uploadFile);
 }
