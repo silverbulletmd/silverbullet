3d3e97f28c17f55a6ee6e1bde6146b0404b57800
Zef Hemel <zef@zef.me>
2024-02-06 16:45:03 +0100

    Fix test

--- common/space_script.test.ts
+++ common/space_script.test.ts
@@ -2,8 +2,11 @@ import { ScriptEnvironment } from "./space_script.ts";
 
 Deno.test("Space script", async () => {
   const env = new ScriptEnvironment();
-  env.evalScript(`
+  env.evalScript(
+    `
 silverbullet.registerFunction("add", (a, b) => a + b);
-    `);
+    `,
+    undefined as any,
+  );
   console.log("Env", env);
 });
