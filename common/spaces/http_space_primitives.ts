04c7a786764c4454048e3ffdd2479dbd9b0dcb9c
Zef Hemel <zef@zef.me>
2024-02-08 21:16:55 +0100

    More moves

--- common/spaces/http_space_primitives.ts
+++ common/spaces/http_space_primitives.ts
@@ -1,6 +1,6 @@
 import { SpacePrimitives } from "./space_primitives.ts";
 import { FileMeta } from "../../type/types.ts";
-import { flushCachesAndUnregisterServiceWorker } from "./sw_util.ts";
+import { flushCachesAndUnregisterServiceWorker } from "../sw_util.ts";
 
 export class HttpSpacePrimitives implements SpacePrimitives {
   constructor(
