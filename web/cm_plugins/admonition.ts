6230c8ab83bdd9408d7021c3c50a0b9086b85cbb
onespaceman <onespaceman@users.noreply.github.com>
2024-03-04 12:35:02 -0500

    Custom admonitions (#786)

--- web/cm_plugins/admonition.ts
+++ web/cm_plugins/admonition.ts
@@ -9,16 +9,14 @@ import {
 import { Client } from "../client.ts";
 import { decoratorStateField, isCursorInRange } from "./util.ts";
 
-type AdmonitionType = "note" | "warning";
-
 const ADMONITION_REGEX =
-  /^>( *)\*{2}(Note|Warning)\*{2}( *)(.*)(?:\n([\s\S]*))?/im;
+  /^>( *)(?:\*{2}|\[!)(.*?)(\*{2}|\])( *)(.*)(?:\n([\s\S]*))?/im;
 const ADMONITION_LINE_SPLIT_REGEX = /\n>/gm;
 
 class AdmonitionIconWidget extends WidgetType {
   constructor(
     readonly pos: number,
-    readonly type: AdmonitionType,
+    readonly type: string,
     readonly editorView: EditorView,
   ) {
     super();
@@ -35,30 +33,14 @@ class AdmonitionIconWidget extends WidgetType {
       });
     });
 
-    switch (this.type) {
-      case "note":
-        outerDiv.insertAdjacentHTML(
-          "beforeend",
-          '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>',
-        );
-        break;
-      case "warning":
-        outerDiv.insertAdjacentHTML(
-          "beforeend",
-          '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10.29 3.86 1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>',
-        );
-        break;
-      default:
-        //
-    }
-
     return outerDiv;
   }
 }
 
 type AdmonitionFields = {
   preSpaces: string;
-  admonitionType: AdmonitionType;
+  admonitionType: string;
+  postSyntax: string
   postSpaces: string;
   admonitionTitle: string;
   admonitionContent: string;
@@ -75,19 +57,25 @@ type AdmonitionFields = {
 // > **note** I am an Admonition Title
 // > admonition text
 //
+// or
+// > [!note] I am an Admonition Title
+// > admonition text
+
 function extractAdmonitionFields(rawText: string): AdmonitionFields | null {
   const regexResults = rawText.match(ADMONITION_REGEX);
 
   if (regexResults) {
     const preSpaces = regexResults[1] || "";
-    const admonitionType = regexResults[2].toLowerCase() as AdmonitionType;
-    const postSpaces = regexResults[3] || "";
-    const admonitionTitle: string = regexResults[4] || "";
-    const admonitionContent: string = regexResults[5] || "";
+    const admonitionType = regexResults[2];
+    const postSyntax = regexResults[3];
+    const postSpaces = regexResults[4] || "";
+    const admonitionTitle: string = regexResults[5] || "";
+    const admonitionContent: string = regexResults[6] || "";
 
     return {
       preSpaces,
       admonitionType,
+      postSyntax,
       postSpaces,
       admonitionTitle,
       admonitionContent,
@@ -117,7 +105,7 @@ export function admonitionPlugin(editor: Client) {
             return;
           }
 
-          const { preSpaces, admonitionType, postSpaces } = extractedFields;
+          const { preSpaces, admonitionType, postSyntax, postSpaces } = extractedFields;
 
           // A blockquote is actually rendered as many divs, one per line.
           // We need to keep track of the `from` offsets here, so we can attach css
@@ -130,35 +118,25 @@ export function admonitionPlugin(editor: Client) {
             accum += line.length + 2;
           });
 
-          // `from` and `to` range info for switching out **info|warning** text with correct
+          // `from` and `to` range info for switching out keyword text with correct
           // icon further down.
           const iconRange = {
             from: from + 1,
-            to: from + preSpaces.length + 2 + admonitionType.length + 2 +
+            to: from + preSpaces.length + 2 + admonitionType.length + postSyntax.length +
               postSpaces.length + 1,
           };
 
           const classes = ["sb-admonition"];
-          switch (admonitionType) {
-            case "note":
-              classes.push("sb-admonition-note");
-              break;
-            case "warning":
-              classes.push("sb-admonition-warning");
-              break;
-            default:
-              //
-          }
 
-          // The first div is the title, attach relevant css classes
+          // The first div is the title, attach title css class
           widgets.push(
             Decoration.line({
-              class: "sb-admonition-title " + classes.join(" "),
+              class: "sb-admonition-title",
             }).range(fromOffsets[0]),
           );
 
-          // If cursor is not within the first line, replace the **note|warning** text
-          // with the correct icon
+          // If cursor is not within the first line, replace the keyword text
+          // with the icon
           if (
             !isCursorInRange(state, [
               from,
@@ -178,10 +156,13 @@ export function admonitionPlugin(editor: Client) {
           }
 
           // Each line of the blockquote is spread across separate divs, attach
-          // relevant css classes here.
-          fromOffsets.slice(1).forEach((fromOffset) => {
+          // relevant css classes and attribute here.
+          fromOffsets.forEach((fromOffset) => {
             widgets.push(
-              Decoration.line({ class: classes.join(" ") }).range(fromOffset),
+              Decoration.line({
+                attributes: { admonition: admonitionType },
+                class: "sb-admonition",
+              }).range(fromOffset),
             );
           });
         }
