0d77e09a326527501ebda14b21c1b34ea28df0dc
MrMugame <mr.mugame@gmail.com>
2024-06-26 00:42:21 +0200

    Hides the '\' from escaped characters

---
+++ web/cm_plugins/escapes.ts
@@ -0,0 +1,28 @@
+import { EditorState } from "@codemirror/state";
+import { syntaxTree } from "@codemirror/language";
+import { Decoration } from "@codemirror/view";
+import {
+  decoratorStateField,
+  invisibleDecoration,
+  isCursorInRange,
+} from "./util.ts";
+
+export function cleanEscapePlugin() {
+  return decoratorStateField(
+    (state: EditorState) => {
+      const widgets: any[] = [];
+
+      syntaxTree(state).iterate({
+        enter({ type, from, to }) {
+          if (
+            type.name === "Escape" &&
+            !isCursorInRange(state, [from, to])
+          ) {
+            widgets.push(invisibleDecoration.range(from, from + 1));
+          }
+        },
+      });
+      return Decoration.set(widgets, true);
+    },
+  );
+}
