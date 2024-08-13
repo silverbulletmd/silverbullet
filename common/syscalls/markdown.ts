307026df27f3101bfb7bb9525143dbd2412cba62
JC Gurango <jc@jcgurango.com>
2024-08-13 20:46:05 +0800

    Expose render parse tree

--- common/syscalls/markdown.ts
+++ common/syscalls/markdown.ts
@@ -1,12 +1,15 @@
 import type { SysCallMapping } from "../../lib/plugos/system.ts";
 import { parse } from "../markdown_parser/parse_tree.ts";
-import type { ParseTree } from "../../plug-api/lib/tree.ts";
+import { type ParseTree, renderToText } from "../../plug-api/lib/tree.ts";
 import { extendedMarkdownLanguage } from "../markdown_parser/parser.ts";
 
 export function markdownSyscalls(): SysCallMapping {
   return {
     "markdown.parseMarkdown": (_ctx, text: string): ParseTree => {
       return parse(extendedMarkdownLanguage, text);
+    },
+    "markdown.renderParseTree": (_ctx, tree: ParseTree): string => {
+      return renderToText(tree);
     },
   };
 }
