010e2b23f4ada77ec57ea770fb142b7c1af84a9a
Marek S. Łukasiewicz <marek@lukasiewicz.tech>
2024-10-20 12:39:58 +0200

    Allow tags to start with a number and be escaped with angle brackets (#1116)
    Includes tests and Markdown/Hashtags page for website. Closes #1072

--- plug-api/lib/tags.ts
+++ plug-api/lib/tags.ts
@@ -4,6 +4,7 @@ import {
   type ParseTree,
   traverseTree,
 } from "@silverbulletmd/silverbullet/lib/tree";
+import { tagRegex } from "$common/markdown_parser/constants.ts";
 
 export function updateITags<T>(obj: ObjectValue<T>, frontmatter: FrontMatter) {
   const itags = [obj.tag, ...frontmatter.tags || []];
@@ -26,7 +27,7 @@ export function extractHashTags(n: ParseTree): string[] {
   const tags = new Set<string>();
   traverseTree(n, (n) => {
     if (n.type === "Hashtag") {
-      tags.add(n.children![0].text!.substring(1));
+      tags.add(extractHashtag(n.children![0].text!));
       return true;
     } else if (n.type === "OrderedList" || n.type === "BulletList") {
       // Don't traverse into sub-lists
@@ -35,6 +36,32 @@ export function extractHashTags(n: ParseTree): string[] {
     return false;
   });
   return [...tags];
+}
+
+/** Extract the name from hashtag text, removing # prefix and <angle brackets> if necessary */
+export function extractHashtag(text: string): string {
+  if (text[0] !== "#") { // you shouldn't call this function at all
+    console.error("extractHashtag called on already clean string", text);
+    return text;
+  } else if (text[1] === "<") {
+    if (text.slice(-1) !== ">") { // this is malformed: #<name but maybe we're trying to autocomplete
+      return text.slice(2);
+    } else { // this is correct #<name>
+      return text.slice(2, -1);
+    }
+  } else { // this is just #name
+    return text.slice(1);
+  }
+}
+
+/** Get markup for a hashtag name with # prefix and angle brackets if necessary */
+export function renderHashtag(name: string): string {
+  // detect with the same regex as the parser
+  const simple: string = "#" + name;
+  const match = simple.match(tagRegex);
+  if (!match || match[0].length !== simple.length) {
+    return `#<${name}>`;
+  } else return simple;
 }
 
 /**
