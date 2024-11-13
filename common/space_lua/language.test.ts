b532b9e3f16320cfd4c5cd255fb87092ef3ebf84
Peter Weston <59482636+p-weston@users.noreply.github.com>
2024-11-13 20:08:24 +0000

    Fix tests for windows paths (#1153)

--- common/space_lua/language.test.ts
+++ common/space_lua/language.test.ts
@@ -3,10 +3,12 @@ import { luaBuildStandardEnv } from "$common/space_lua/stdlib.ts";
 import { LuaEnv, LuaStackFrame } from "$common/space_lua/runtime.ts";
 import { evalStatement } from "$common/space_lua/eval.ts";
 import { assert } from "@std/assert/assert";
+import { fileURLToPath } from "node:url";
+
 Deno.test("Lua language tests", async () => {
   // Read the Lua file
   const luaFile = await Deno.readTextFile(
-    new URL("./language_test.lua", import.meta.url).pathname,
+    fileURLToPath(new URL("./language_test.lua", import.meta.url)),
   );
   const chunk = parse(luaFile, {});
   const env = new LuaEnv(luaBuildStandardEnv());
