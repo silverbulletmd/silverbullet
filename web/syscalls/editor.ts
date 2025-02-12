3e0a75429ffabd4d44261b775235a49cd2b72324
MrMugame <mr.mugame@gmail.com>
2025-02-12 20:40:03 +0100

    Update editor syscalls

--- web/syscalls/editor.ts
+++ web/syscalls/editor.ts
@@ -19,7 +19,10 @@ import { getCM as vimGetCm, Vim } from "@replit/codemirror-vim";
 import type { SysCallMapping } from "$lib/plugos/system.ts";
 import type { FilterOption } from "@silverbulletmd/silverbullet/type/client";
 import type { PageMeta, UploadFile } from "../../plug-api/types.ts";
-import type { PageRef } from "@silverbulletmd/silverbullet/lib/page_ref";
+import {
+  type LocationRef,
+  parseLocationRef,
+} from "@silverbulletmd/silverbullet/lib/page_ref";
 import { openSearchPanel } from "@codemirror/search";
 
 export function editorSyscalls(client: Client): SysCallMapping {
@@ -28,7 +31,7 @@ export function editorSyscalls(client: Client): SysCallMapping {
       return client.currentPage;
     },
     "editor.getCurrentPageMeta": (): PageMeta | undefined => {
-      return client.ui.viewState.currentPageMeta;
+      return client.ui.viewState.current?.meta;
     },
     "editor.getText": () => {
       return client.editorView.state.sliceDoc();
@@ -47,14 +50,14 @@ export function editorSyscalls(client: Client): SysCallMapping {
     },
     "editor.navigate": async (
       _ctx,
-      pageRef: PageRef | string,
+      locationRef: LocationRef | string,
       replaceState = false,
       newWindow = false,
     ) => {
-      if (typeof pageRef === "string") {
-        pageRef = { page: pageRef };
+      if (typeof locationRef === "string") {
+        locationRef = parseLocationRef(locationRef);
       }
-      await client.navigate(pageRef, replaceState, newWindow);
+      await client.navigate(locationRef, replaceState, newWindow);
     },
     "editor.reloadPage": async () => {
       await client.reloadPage();
