31792a3a432f792f2228f1ec1cd85f8eea3d3c10
KitAmbraid <128903658+KitAmbraid@users.noreply.github.com>
2025-02-22 12:00:54 +0100

    space lua: os.date add support for %U, %V and %W (#1250)

--- common/space_lua/stdlib/os_test.lua
+++ common/space_lua/stdlib/os_test.lua
@@ -6,4 +6,8 @@ end
 
 -- Basic OS functions
 assert(os.time() > 0)
-assert(os.date("%Y-%m-%d", os.time({ year = 2020, month = 1, day = 1 })) == "2020-01-01") 
+assert(os.date("%Y-%m-%d", os.time({ year = 2020, month = 1, day = 1 })) == "2020-01-01")
+
+-- Week calculations
+assert(os.date("%U %V %W", os.time({ year = 2051, month = 1, day = 1 })) == "01 52 00")
+assert(os.date("%U %V %W", os.time({ year = 2025, month = 2, day = 18})) == "07 08 07")
