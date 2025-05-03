19806e31a863e44e67e7bed3843907ddfc8080a7
Lennart Ochel <lennart.ochel@outlook.com>
2025-05-03 13:27:13 +0200

    Test os.difftime

--- common/space_lua/stdlib/os_test.lua
+++ common/space_lua/stdlib/os_test.lua
@@ -10,3 +10,8 @@ assert(os.date("%Y-%m-%d", os.time({ year = 2020, month = 1, day = 1 })) == "202
 
 -- Week calculations
 assert(os.date("%U %V %W", os.time({ year = 2051, month = 1, day = 1 })) == "01 52 00")
+
+local t = os.time()
+assert(os.difftime(t+10, t) == 10)
+assert(os.difftime(t, t+10) == -10)
+assert(os.difftime(t, t) == 0)
