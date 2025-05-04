ea3d6928cf7b63eb2174a4c3fb52b9e74b000556
Matthias Benaets <89214559+MatthiasBenaets@users.noreply.github.com>
2025-05-04 20:42:00 +0200

    feat: configure vim in CONFIG (#1369)

--- type/client.ts
+++ type/client.ts
@@ -46,3 +46,12 @@ export type SmartQuotesConfig = {
   double?: { left?: string; right?: string };
   single?: { left?: string; right?: string };
 };
+
+type vimMode = "normal" | "insert" | "visual";
+
+export type VimConfig = {
+  unmap?: (string | { key: string; mode?: vimMode })[];
+  map?: { map: string; to: string; mode?: vimMode }[];
+  noremap?: { map: string; to: string; mode?: vimMode }[];
+  commands?: { ex: string; command: string }[];
+};
