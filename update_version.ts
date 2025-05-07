82014d7bfc9b52460e1e88007b4453c34487acc8
Lennart Ochel <lennart.ochel@outlook.com>
2025-05-07 18:52:07 +0200

    Generate version from git describe at build time (#1374)
    * Generate version from git describe at build time
    
    * Add 2.0-beta prefix
    
    * Use Deno.Command instead of Deno.run

---
+++ update_version.ts
@@ -0,0 +1,16 @@
+export async function updateVersionFile() {
+  const command = new Deno.Command("git", {
+    args: ["describe", "--tags", "--long", "--dirty"],
+    stdout: "piped",
+    stderr: "piped",
+  });
+
+  const { stdout } = await command.output();
+  const commitVersion = new TextDecoder().decode(stdout).trim();
+
+  const versionFilePath = "./version.ts";
+  const versionContent = `export const version = "2.0-beta (${commitVersion})";\n`;
+
+  await Deno.writeTextFile(versionFilePath, versionContent);
+  console.log(`Updated version.ts with version information: ${commitVersion}`);
+}
