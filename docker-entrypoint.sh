90ed671c2c2cc3612453cfdac26ea258e0990a43
Zef Hemel <zef@zef.me>
2025-06-18 19:24:29 +0200

    Fixes #1423

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,8 +30,8 @@ if [ "$PUID" == "0" ] || [ "$UID" != "0" ]; then
     /silverbullet $@
 else
     # Create silverbullet user and group ad-hoc mapped to PUID and PGID
-    getent group $PGID &> /dev/null || addgroup -g $PGID silverbullet
-    getent passwd $PUID &> /dev/null || adduser -D -H -u $PUID -g $PGID silverbullet
+    getent group $PGID &> /dev/null || groupadd -g $PGID silverbullet
+    getent passwd $PUID &> /dev/null || useradd -M -u $PUID -g $PGID silverbullet
     args="$@"
     # And run via su as requested PUID, usually this will be 'silverbullet' but if a user with this idea already exists, we will use that
     USERNAME=$(getent passwd $PUID | cut -d ":" -f 1)
