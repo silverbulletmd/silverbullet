806bd03ed50e99f4d46674d2a39fdbd281209dba
Lennart Ochel <lennart.ochel@outlook.com>
2025-08-01 23:21:28 +0200

    Fix os.time (#1481)

--- lib/space_lua/stdlib/os_test.lua
+++ lib/space_lua/stdlib/os_test.lua
@@ -22,3 +22,6 @@ assert(os.date("%U %V %W", os.time({ year = 2025, month = 1, day = 7 })) == "01 
 assert(os.date("%U %V %W", os.time({ year = 2025, month = 5, day = 3 })) == "17 18 17")
 
 assert(os.date("%w", 1750672800) == "1")
+
+-- https://community.silverbullet.md/t/incorrect-timestamp-returned-by-os-time/2682
+assert_equal(os.date("%Y-%m-%d", os.time({year=2025, month=2, day=3})), "2025-02-03")
