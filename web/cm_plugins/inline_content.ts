ecf2f45c09242afb7103e560b7bc28787c75b8fc
MrMugame <mr.mugame@gmail.com>
2025-08-20 23:35:22 +0200

    [Major] Inline content rewrite

--- web/cm_plugins/inline_content.ts
+++ web/cm_plugins/inline_content.ts
@@ -1,6 +1,6 @@
 import type { EditorState, Range } from "@codemirror/state";
 import { syntaxTree } from "@codemirror/language";
-import { Decoration, WidgetType } from "@codemirror/view";
+import { Decoration } from "@codemirror/view";
 import {
   decoratorStateField,
   invisibleDecoration,
@@ -13,127 +13,147 @@ import {
   resolveMarkdownLink,
 } from "@silverbulletmd/silverbullet/lib/resolve";
 import {
+  getNameFromPath,
+  getPathExtension,
   isMarkdownPath,
   parseToRef,
 } from "@silverbulletmd/silverbullet/lib/ref";
-import { mime } from "mimetypes";
 import { LuaWidget } from "./lua_widget.ts";
+import { mdLinkRegex, wikiLinkRegex } from "../markdown_parser/constants.ts";
+import { mime } from "mimetypes";
 
 type ContentDimensions = {
   width?: number;
   height?: number;
 };
 
-class InlineContentWidget extends WidgetType {
-  constructor(
-    readonly url: string,
-    readonly title: string,
-    readonly dim: ContentDimensions | undefined,
-    readonly client: Client,
-  ) {
-    super();
-  }
-
-  override get estimatedHeight(): number {
-    return this.client.getCachedWidgetHeight(
-      `content:${this.url}`,
-    );
-  }
-
-  override eq(other: InlineContentWidget) {
-    return other.url === this.url && other.title === this.title &&
-      JSON.stringify(other.dim) === JSON.stringify(this.dim);
-  }
-
-  toDOM() {
-    const div = document.createElement("div");
-    div.className = "sb-inline-content";
-    div.style.display = "block";
-    const mimeType = mime.getType(
-      this.url.substring(this.url.lastIndexOf(".") + 1),
-    );
-
-    if (!mimeType) {
-      return div;
+async function inlineHtmlFromURL(
+  client: Client,
+  url: string,
+  alias: string,
+  dimensions: ContentDimensions | undefined,
+): Promise<HTMLElement | string> {
+  let mimeType: string | null | undefined;
+  if (!isLocalURL(url)) {
+    const response = await fetch(url, { method: "HEAD" });
+    if (!response.ok) {
+      return `Failed to fetch resource, server responded with status code: ${response.status}`;
     }
 
-    let url = this.url;
-
-    // If the URL is a local path, encode the : so that it's not interpreted as a protocol
-    if (isLocalURL(url)) {
-      url = url.replace(":", "%3A");
+    mimeType = response.headers.get("Content-Type");
+  } else {
+    const ref = parseToRef(url);
+    if (!ref) {
+      return `Failed to parse url`;
     }
 
-    if (mimeType.startsWith("image/")) {
-      const img = document.createElement("img");
-      img.src = url;
-      img.alt = this.title;
-      this.setDim(img, "load");
-      div.appendChild(img);
-    } else if (mimeType.startsWith("video/")) {
-      const video = document.createElement("video");
-      video.src = url;
-      video.title = this.title;
-      video.controls = true;
-      video.autoplay = false;
-      this.setDim(video, "loadeddata");
-      div.appendChild(video);
-    } else if (mimeType.startsWith("audio/")) {
-      const audio = document.createElement("audio");
-      audio.src = url;
-      audio.title = this.title;
-      audio.controls = true;
-      audio.autoplay = false;
-      this.setDim(audio, "loadeddata");
-      div.appendChild(audio);
-    } else if (mimeType === "application/pdf") {
-      const embed = document.createElement("object");
-      embed.type = mimeType;
-      embed.data = url;
-      embed.style.width = "100%";
-      embed.style.height = "20em";
-      this.setDim(embed, "load");
-      div.appendChild(embed);
-    }
-
-    return div;
+    mimeType = mime.getType(getPathExtension(ref.path));
   }
 
-  setDim(el: HTMLElement, event: string) {
-    const cachedContentHeight = this.client.getCachedWidgetHeight(
-      `content:${this.url}`,
+  if (!mimeType) {
+    return `Failed to determine mime type`;
+  }
+
+  const setDimension = (element: HTMLElement, event: string) => {
+    const cachedContentHeight = client.getCachedWidgetHeight(
+      `content:${url}`,
     );
 
-    el.addEventListener(event, () => {
-      if (el.clientHeight !== cachedContentHeight) {
-        this.client.setCachedWidgetHeight(
-          `content:${this.url}`,
-          el.clientHeight,
+    element.addEventListener(event, () => {
+      if (element.clientHeight !== cachedContentHeight) {
+        client.setCachedWidgetHeight(
+          `content:${url}`,
+          element.clientHeight,
         );
       }
     });
 
-    el.style.maxWidth = "100%";
+    element.style.maxWidth = "100%";
 
-    if (this.dim) {
-      if (this.dim.height) {
-        el.style.height = `${this.dim.height}px`;
+    if (dimensions) {
+      if (dimensions.height) {
+        element.style.height = `${dimensions.height}px`;
       }
-      if (this.dim.width) {
-        el.style.width = `${this.dim.width}px`;
+      if (dimensions.width) {
+        element.style.width = `${dimensions.width}px`;
       }
     } else if (cachedContentHeight > 0) {
-      el.style.height = cachedContentHeight.toString();
+      element.style.height = cachedContentHeight.toString();
     }
+  };
+
+  // If the URL is a local, encode the : so that it's not interpreted as a protocol
+  const sanitizedURL = isLocalURL(url) ? url.replace(":", "%3A") : url;
+
+  let result: HTMLElement | string;
+  if (mimeType.startsWith("image/")) {
+    const img = document.createElement("img");
+    img.src = sanitizedURL;
+    img.alt = alias;
+    setDimension(img, "load");
+    result = img;
+  } else if (mimeType.startsWith("video/")) {
+    const video = document.createElement("video");
+    video.src = sanitizedURL;
+    video.title = alias;
+    video.controls = true;
+    video.autoplay = false;
+    setDimension(video, "loadeddata");
+    result = video;
+  } else if (mimeType.startsWith("audio/")) {
+    const audio = document.createElement("audio");
+    audio.src = sanitizedURL;
+    audio.title = alias;
+    audio.controls = true;
+    audio.autoplay = false;
+    setDimension(audio, "loadeddata");
+    result = audio;
+  } else if (mimeType === "application/pdf") {
+    const embed = document.createElement("object");
+    embed.type = mimeType;
+    embed.data = sanitizedURL;
+    embed.style.width = "100%";
+    embed.style.height = "20em";
+    setDimension(embed, "load");
+    result = embed;
+  } else if (mimeType === "text/markdown") {
+    if (!isLocalURL(url)) {
+      const response = await fetch(url);
+      if (!response.ok) {
+        // This shouldn't really happen, but let's check anyways
+        return `Couldn't transclude markdown from external source. Server responded with: ${response.status}`;
+      }
+
+      result = await response.text();
+    } else {
+      const ref = parseToRef(url);
+
+      if (!ref || !isMarkdownPath(ref.path)) {
+        // We can be fairly sure this can't happen, but just be sure
+        return `Couldn't transclude markdown, invalid path`;
+      }
+
+      try {
+        ({ text: result } = await client.space.readPage(
+          getNameFromPath(ref.path),
+        ));
+      } catch {
+        result = `Couldn't transclude markdown, page doesn't exist`;
+      }
+    }
+  } else {
+    result = `Server responded with unsupported mimeType: ${mimeType}`;
   }
+
+  return result;
 }
 
 // Parse an alias, possibly containing dimensions into an object
 // Formats supported: "alias", "alias|100", "alias|100x200", "100", "100x200"
 function parseAlias(
   text: string,
-): { alias?: string; dim?: ContentDimensions } {
-  let alias: string | undefined;
+): { alias: string; dim?: ContentDimensions } {
+  let alias: string;
   let dim: ContentDimensions | undefined;
   if (text.includes("|")) {
     const [aliasPart, dimPart] = text.split("|");
@@ -155,6 +175,7 @@ function parseAlias(
     if (height) {
       dim.height = parseInt(height);
     }
+    alias = "";
   } else {
     alias = text;
   }
@@ -171,92 +192,75 @@ export function inlineContentPlugin(client: Client) {
     }
 
     syntaxTree(state).iterate({
-      enter: (node) => {
-        if (node.name !== "Image") {
+      enter: ({ type, from, to }) => {
+        if (type.name !== "Image") {
           return;
         }
 
-        const text = state.sliceDoc(node.from, node.to);
-        let [url, alias]: (string | undefined)[] = [undefined, undefined];
-        let match: RegExpExecArray | null;
-        if ((match = /!?\[([^\]]*)\]\((.+)\)/g.exec(text))) {
-          [/* fullMatch */, alias, url] = match;
-        } else if (
-          (match = /(!?\[\[)([^\]\|]+)(?:\|([^\]]+))?(\]\])/g.exec(text))
-        ) {
-          [/* fullMatch */, /* firstMark */ , url, alias] = match;
-          url = "/" + url;
-        }
-        if (!url) {
-          return;
-        }
+        const text = state.sliceDoc(from, to);
 
-        let dim: ContentDimensions | undefined;
-        if (alias) {
-          const { alias: parsedAlias, dim: parsedDim } = parseAlias(alias);
-          if (parsedAlias) {
-            alias = parsedAlias;
+        let url, alias = undefined;
+
+        mdLinkRegex.lastIndex = 0;
+        wikiLinkRegex.lastIndex = 0;
+        let match: RegExpMatchArray | null = null;
+        if ((match = mdLinkRegex.exec(text)) && match.groups) {
+          ({ url, title: alias } = match.groups);
+
+          if (isLocalURL(url)) {
+            url = resolveMarkdownLink(
+              client.currentName(),
+              decodeURI(url),
+            );
           }
-          dim = parsedDim;
+        } else if ((match = wikiLinkRegex.exec(text)) && match.groups) {
+          ({ stringRef: url, alias } = match.groups);
+        } else {
+          // We found no match
+          return;
+        }
+
+        let dimension: ContentDimensions | undefined;
+        if (alias) {
+          ({ alias, dim: dimension } = parseAlias(alias));
         } else {
           alias = "";
         }
 
-        if (isLocalURL(url)) {
-          url = resolveMarkdownLink(
-            client.currentName(),
-            decodeURI(url),
-          );
-          const ref = parseToRef(url);
-          if (
-            ref &&
-            isMarkdownPath(ref.path) &&
-            client.clientSystem.allKnownFiles.has(ref.path)
-          ) {
-            widgets.push(
-              Decoration.widget({
-                widget: new LuaWidget(
-                  client,
-                  `widget:${client.currentPath()}:${ref.path}`,
-                  ref.path,
-                  async (pageName) => {
-                    const { text } = await client.space.readPage(pageName);
-                    return {
-                      _isWidget: true,
-                      cssClasses: ["sb-markdown-widget-inline"],
-                      markdown: text,
-                    };
-                  },
-                  true,
-                  true,
-                ),
-                block: true,
-              }).range(node.to + 1),
-            );
-
-            if (!isCursorInRange(state, [node.from, node.to])) {
-              widgets.push(invisibleDecoration.range(node.from, node.to));
-            }
-
-            return;
-          }
+        if (!isCursorInRange(state, [from, to])) {
+          widgets.push(invisibleDecoration.range(from, to));
         }
 
         widgets.push(
           Decoration.widget({
-            widget: new InlineContentWidget(
-              url,
-              alias,
-              dim,
+            widget: new LuaWidget(
               client,
+              `widget:${client.currentPath()}:${text}`,
+              text,
+              async () => {
+                const result = await inlineHtmlFromURL(
+                  client,
+                  url,
+                  alias,
+                  dimension,
+                );
+                const content = typeof result === "string"
+                  ? { markdown: result }
+                  : { html: result };
+
+                return {
+                  _isWidget: true,
+                  display: "block",
+                  cssClasses: ["sb-inline-content"],
+                  ...content,
+                };
+              },
+              true,
+              true,
             ),
             block: true,
-          }).range(node.to + 1),
+          }).range(to + 1),
         );
-
-        if (!isCursorInRange(state, [node.from, node.to])) {
-          widgets.push(invisibleDecoration.range(node.from, node.to));
-        }
       },
     });
 
