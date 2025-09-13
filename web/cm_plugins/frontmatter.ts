071d58826e54386259dc7477b40b4a0a3eba7b5c
Andy Costanza <andy.costanza@pm.me>
2025-09-13 14:53:12 +0200

    feat: make Internal links (WikiLinks [[...]])  navigable in a frontmatter block

--- web/cm_plugins/frontmatter.ts
+++ web/cm_plugins/frontmatter.ts
@@ -99,6 +99,124 @@ export function frontmatterPlugin(client: Client) {
                 }).range(mFrom, mTo),
               );
             }
+
+            // 2) Internal links: WikiLinks [[...]] (make navigable)
+            wikiLinkRegex.lastIndex = 0;
+            let wMatch: RegExpExecArray | null;
+            while ((wMatch = wikiLinkRegex.exec(text)) !== null) {
+              if (!wMatch || !wMatch.groups) {
+                return;
+              }
+              const mFrom = from + (wMatch.index ?? 0);
+              const mTo = mFrom + wMatch[0].length;
+              if (isCursorInRange(state, [mFrom, mTo])) {
+                continue;
+              }
+
+              const {leadingTrivia, stringRef, alias, trailingTrivia} =
+                wMatch.groups;
+
+              const ref = parseToRef(stringRef);
+              let linkStatus: "file-missing" | "default" | "invalid" = "default";
+              if (!ref) {
+                linkStatus = "invalid";
+              } else if (ref.path === "" || isBuiltinPath(ref.path)) {
+                linkStatus = "default";
+              } else if (
+                Array.from(client.clientSystem.allKnownFiles)
+                  .some((file) => file === ref.path)
+              ) {
+                linkStatus = "default";
+              } else if (client.fullSyncCompleted) {
+                linkStatus = "file-missing";
+              }
+              let css = {
+                "file-missing": "sb-wiki-link-missing",
+                "invalid": "sb-wiki-link-invalid",
+                "default": "",
+              }[linkStatus];
+
+              const renderingSyntax =
+                client.ui.viewState.uiOptions.markdownSyntaxRendering;
+              if (isCursorInRange(state, [from, to]) || renderingSyntax) {
+                // Only attach a CSS class, then get out
+                if (linkStatus !== "default") {
+                  widgets.push(
+                    Decoration.mark({
+                      class: css,
+                    }).range(from + leadingTrivia.length, to - trailingTrivia.length),
+                  );
+                }
+
+                return;
+              }
+              const cleanedPath = ref ? getNameFromPath(ref.path) : stringRef;
+              const helpText = {
+                "default": `Navigate to ${cleanedPath}`,
+                "file-missing": `Create ${cleanedPath}`,
+                "invalid": `Cannot create invalid file ${cleanedPath}`,
+              }[linkStatus];
+
+              let linkText = alias || stringRef;
+
+              // The `&& ref` is only there to make typescript happy
+              if (linkStatus === "default" && ref) {
+                const meta = client.ui.viewState.allPages.find((p) =>
+                  parseToRef(p.ref)?.path === ref.path
+                );
+
+                const renderedRef = structuredClone(ref);
+
+                // We don't want to render the meta
+                renderedRef.meta = false;
+                // We also don't want to rendered the prefix of the path
+                renderedRef.path = fileName(renderedRef.path);
+
+                const prefix = (ref.details?.type === "position" ||
+                  ref.details?.type === "linecolumn")
+                  ? ""
+                  : (meta?.pageDecoration?.prefix ?? "");
+
+                linkText = alias || (prefix + encodeRef(renderedRef));
+
+                if (meta?.pageDecoration?.cssClasses) {
+                  css += " sb-decorated-object " +
+                    meta.pageDecoration.cssClasses
+                      .join(" ")
+                      .replaceAll(/[^a-zA-Z0-9-_ ]/g, "");
+                }
+              }
+
+              widgets.push(
+                Decoration.replace({
+                  widget: new LinkWidget({
+                    text: linkText,
+                    title: helpText,
+                    href: ref ? encodePageURI(encodeRef(ref)) : undefined,
+                    cssClass: "sb-wiki-link " + css,
+                    from: mFrom,
+                    callback: (e) => {
+                      if (e.altKey) {
+                        // Move cursor into the link's content
+                        client.editorView.dispatch({
+                          selection: {anchor: mFrom + leadingTrivia.length},
+                        });
+                        client.focus();
+                        return;
+                      }
+                      const clickEvent: ClickEvent = {
+                        page: client.currentName(),
+                        ctrlKey: e.ctrlKey,
+                        metaKey: e.metaKey,
+                        altKey: e.altKey,
+                        pos: mFrom,
+                      };
+                      client.dispatchClickEvent(clickEvent).catch(console.error);
+                    },
+                  }),
+                }).range(mFrom, mTo),
+              );
+            }
           }
         },
       });
