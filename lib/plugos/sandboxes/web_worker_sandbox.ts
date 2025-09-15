ad39158cd17e2010e7f3a9e0adda6843d40fb9ca
MrMugame <40832361+MrMugame@users.noreply.github.com>
2025-09-15 08:54:55 +0200

    Separate plug name and file name (#1541)
    * Separate plug name from file path
    
    Plug name and plug path were very intertwined in the code. This could
    cause problems, if the file path and plug name didn't align.
    The plug manager also modified the plug path for some plugs, which could
    be even more confusing.
    
    * Code improvements for the plug manager
    
    * Be more rigorous about failure
    
    * Also allow `silverbullet-name` case again

--- lib/plugos/sandboxes/web_worker_sandbox.ts
+++ lib/plugos/sandboxes/web_worker_sandbox.ts
@@ -1,7 +1,17 @@
 import { WorkerSandbox } from "./worker_sandbox.ts";
 import type { Plug } from "../plug.ts";
 import type { SandboxFactory } from "./sandbox.ts";
+import { fsEndpoint } from "../../spaces/constants.ts";
 
-export function createSandbox<HookT>(workerUrl: URL): SandboxFactory<HookT> {
-  return (plug: Plug<HookT>) => new WorkerSandbox(plug, workerUrl);
+export function createWorkerSandboxFromLocalPath<HookT>(
+  name: string,
+): SandboxFactory<HookT> {
+  return (plug: Plug<HookT>) =>
+    new WorkerSandbox(
+      plug,
+      new URL(
+        name,
+        document.baseURI.slice(0, -1) + fsEndpoint + "/", // We're NOT striping trailing '/', this used to be `location.origin`
+      ),
+    );
 }
