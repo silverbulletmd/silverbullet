d63c7e49a7dc786d1ad40aa77d3b83232cf7f94a
Zef Hemel <zef@zef.me>
2025-09-29 15:56:41 +0200

    Disable js.import tests for now

--- client/space_lua/stdlib/js_test.lua
+++ client/space_lua/stdlib/js_test.lua
@@ -1,14 +1,16 @@
 local function assert_equal(a, b)
-  if a ~= b then
-      error("Assertion failed: " .. a .. " is not equal to " .. b)
-  end
+    if a ~= b then
+        error("Assertion failed: " .. a .. " is not equal to " .. b)
+    end
 end
 
-local lodash = js.import("https://esm.sh/lodash@4.17.21")
+-- Disabling these for now, too dependent on flaky esm.sh
 
-assert_equal(js.stringify(lodash.chunk({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3)), '[[1,2,3],[4,5,6],[7,8,9],[10]]')
+-- local lodash = js.import("https://esm.sh/lodash@4.17.21")
 
-local moment = js.import("https://esm.sh/moment@2.30.1")
+-- assert_equal(js.stringify(lodash.chunk({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3)), '[[1,2,3],[4,5,6],[7,8,9],[10]]')
 
-local day = moment("1995-12-25");
-assert_equal(day.format("DD-MM-YYYY"), "25-12-1995")
+-- local moment = js.import("https://esm.sh/moment@2.30.1")
+
+-- local day = moment("1995-12-25");
+-- assert_equal(day.format("DD-MM-YYYY"), "25-12-1995")
