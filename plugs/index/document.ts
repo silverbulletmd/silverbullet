c2d7dfbdf83700ab019614bd35220178b8967cc4
Zef Hemel <zef@zef.me>
2026-01-29 14:29:52 +0100

    Exclude `.plug.js` and `.js.map` files from document picker (messy).

--- plugs/index/document.ts
+++ plugs/index/document.ts
@@ -1,12 +1,15 @@
-import { index, space, system } from "@silverbulletmd/silverbullet/syscalls";
-import type { DocumentMeta } from "../../plug-api/types/index.ts";
+import { index, space } from "@silverbulletmd/silverbullet/syscalls";
+import type { DocumentMeta } from "@silverbulletmd/silverbullet/type/index";
+
+const nonDocumentFileExtensions = [".md", ".plug.js", ".js.map"];
 
 // Note: clearFileIndex is not called but since this is the only document:index listener, this should be fine (famous last words)
 export async function indexDocument(name: string) {
-  if (await system.getMode() === "ro") {
+  // Check if path doesn't end with a non-document file extension
+  if (nonDocumentFileExtensions.find((ext) => name.endsWith(ext))) {
     return;
   }
-  // console.log("Indexing document", name);
+
   const fileMeta = await space.getDocumentMeta(name);
   await index.indexObjects<DocumentMeta>(fileMeta.name, [fileMeta]);
 }
