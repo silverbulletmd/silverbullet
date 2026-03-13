dbf601f8ba73e673d9554b8b35dd3b28849fa2ea
Brett Anthoine <github@anthoine.ch>
2026-03-13 19:40:58 +0100

    Add optional stdin to shell syscall

--- plug-api/syscalls/shell.ts
+++ plug-api/syscalls/shell.ts
@@ -4,11 +4,14 @@ import { syscall } from "../syscall.ts";
  * Runs a shell command.
  * @param cmd the command to run
  * @param args the arguments to pass to the command
+ * @param stdin optional string to pass as stdin to the command
  * @returns the stdout, stderr, and exit code of the command
  */
 export function run(
   cmd: string,
   args: string[],
+  stdin?: string,
 ): Promise<{ stdout: string; stderr: string; code: number }> {
-  return syscall("shell.run", cmd, args);
+  return syscall("shell.run", cmd, args, stdin);
 }
+
