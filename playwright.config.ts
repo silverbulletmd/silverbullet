b1df359b697b50de3ca76dc8de016d3a27e5734b
Zef Hemel <zef@zef.me>
2026-04-08 20:04:00 +0200

    Run e2e tests on Firefox as well

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,5 +18,9 @@ export default defineConfig({
 			name: "chromium",
 			use: { ...devices["Desktop Chrome"] },
 		},
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
 	],
 });
