740a01f481f6b5ed49fff94794c7613feb2d0143
Zef Hemel <zef@zef.me>
2026-06-03 11:28:58 +0200

    Rust: basic working silverbullet binary server

--- common/src/space.rs
+++ common/src/space.rs
@@ -3,10 +3,12 @@
 pub mod disk;
 pub mod embed;
 pub mod http;
+pub mod readonly;
 
 pub use disk::DiskSpacePrimitives;
 pub use embed::{EmptySpacePrimitives, FallthroughSpacePrimitives, ReadOnlyDirSpacePrimitives};
 pub use http::HttpSpacePrimitives;
+pub use readonly::ReadOnlySpacePrimitives;
 
 #[cfg(any(test, feature = "testing"))]
 pub mod memory;
